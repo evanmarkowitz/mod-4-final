eb3aaf021ead39b8e30e04407cbedc8add028cc8
Evan Markowitz <emarkowitz1@gmail.com>
2019-09-15 12:46:22 -0600

    Make fetch based off query

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="searchInput">
     <input v-model="query">
-    <button v-on:click="fetchCurrentQuery('hi')">Find Images</button>
+    <button v-on:click="fetchCurrentQuery(query)">Find Images</button>
   </div>
 </template>
 <script>
   // const fetchImages = require("../../apiCalls/apiCalls");
   import fetchImages from "../../apiCalls/apiCalls"
   const buildUrl = (searchQuery) => {
-    return `https://api.unsplash.com/search/photos?page=1&query=hi`
+    return `https://api.unsplash.com/search/photos?page=1&query=${searchQuery}`
   }
   export default {
     name: 'SearchInput',
@@ -18,14 +18,13 @@
       }
   },
     methods: {
-      fetchCurrentQuery: function (query) {
-        console.log('hi')
-        const url = buildUrl(query)
+      fetchCurrentQuery: async function (query) {
+        const url = await buildUrl(query)
         try{
-          let response = fetchImages(url)
-          console.log(response)
+          let response = await fetchImages(url)
+          await console.log(response)
         } catch(error) {
-          console.log(error)
+          await console.log(error)
         }
       }, 
     }
