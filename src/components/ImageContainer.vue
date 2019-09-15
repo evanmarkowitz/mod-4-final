5cdf67df19243d255c483a3d7c65378e63d6f001
Evan Markowitz <emarkowitz1@gmail.com>
2019-09-15 15:41:19 -0600

    Add loop through data and styling for Image Container

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    {{data}}
+  <section class='columns'>
+    <img v-for="image in data" :src="image" :alt="alt" class='image'/>
   </section>
 </template>
 
@@ -15,5 +15,19 @@ export default {
 </script>
 
 <style>
+
+.columns{
+  box-sizing: border-box;
+  padding: 1rem;
+  columns: 5 250px;
+  column-gap: 1rem;
+}
+
+.image{
+
+  margin: 0 1rem 1rem 0;
+  display: inline-block;
+  width: 100%;
+}
 
 </style>
